e6d5271135507d273a7b21af34980027818a1d7a
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-08-11 20:48:07 +0300

    Add project files.

---
+++ Constructor/Bebek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Constructor
+{
+    internal class Bebek
+    {
+       
+        public DateTime DogumTarihi { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        // Default Constructor
+        public Bebek()
+        {
+            DogumTarihi = DateTime.Now; // Doğum tarihi o anki tarih olarak atanır
+            Console.WriteLine("Ingaaaa"); // Mesajı ekrana yazdır
+        }
+
+        // Parametreli Constructor
+        public Bebek(string ad, string soyad) : this() // Default constructor'ı çağır
+        {
+            Ad = ad;
+            Soyad = soyad;
+        }
+
+        // Bebek bilgilerini yazdırma metodu
+        public void BebekBilgileri()
+        {
+            Console.WriteLine($"Adı: {Ad}");
+            Console.WriteLine($"Soyadı: {Soyad}");
+            Console.WriteLine($"Doğum Tarihi: {DogumTarihi.ToShortDateString()}");
+        }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Bebek bebek1 = new Bebek();
+            bebek1.BebekBilgileri();
+
+           
+            Bebek bebek2 = new Bebek("Çağatay", "Kızıl");
+            bebek2.BebekBilgileri();
+        }
+    }
+}
